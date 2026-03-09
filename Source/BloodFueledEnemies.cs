589fb435bf3aa374881178c23ee0e90bc333defc
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-08 23:15:32 -0700

    Initial Commit

---
+++ Source/BloodFueledEnemies.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace Nyxpiri.ULTRAKILL.BloodFueledEnemies
+{
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.blood-fueled-enemies", "Blood Fueled Enemies", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class BloodFueledEnemies: BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+    }
+}
